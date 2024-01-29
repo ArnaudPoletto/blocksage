ba74da5927e34a6a59b7c124980888deb5df2cc6
ArnaudPoletto <arnaud.poletto@epfl.ch>
2024-01-29 14:24:56 +0100

    add parallel processing of dataset

---
+++ script/cluster_dataset_from_mcas.bat
@@ -0,0 +1,7 @@
+(
+    cd ..\src\mca
+    conda activate dm
+    conda run -n dm --no-capture-output --live-stream python cluster_dataset_from_mcas.py --cluster_size=3 --cluster_stride=1 --parallelize --max_concurrent_processes=20
+    conda deactivate
+    cd ..\..\script
+)
